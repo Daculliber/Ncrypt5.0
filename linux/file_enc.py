a5e0c11bc51c5842ad025609bed459caa9f64f8c
Daculliber <daculliber165@gmail.com>
2025-01-03 20:44:27 +0200

    Add files via upload

---
+++ linux/file_enc.py
@@ -0,0 +1,112 @@
+from cryptography.fernet import Fernet
+from func import lst_about
+from file_explore import explore
+import os,time
+# This program uses the 'cryptography' library: https://cryptography.io/
+def clear():
+    os.system ('clear')
+
+def gen_key():
+	key = Fernet.generate_key()
+	return key
+def enc(inp,key):
+	# using the generated key
+	fernet = Fernet(key)
+	# opening the original file to encrypt
+	with open(inp, 'rb') as file:
+		original = file.read()
+		file.close
+		
+	# encrypting the file
+	encrypted = fernet.encrypt(original)
+
+	# opening the file in write mode and 
+	# writing the encrypted data
+	with open(inp, 'wb') as encrypted_file:
+		encrypted_file.write(encrypted)
+		file.close
+def dec(inp,key):
+	# using the key
+	fernet = Fernet(key)
+
+	# opening the encrypted file
+	with open(inp, 'rb') as enc_file:
+		encrypted = enc_file.read()
+
+	# decrypting the file
+	decrypted = fernet.decrypt(encrypted)
+
+	# opening the file in write mode and
+	# writing the decrypted data
+	with open(inp, 'wb') as dec_file:
+		dec_file.write(decrypted)
+def pin_enc(inp,key,pin):
+	for i in range (pin):
+		enc(inp,key)
+def pin_dec(inp,key,pin):
+	for i in range(pin):
+		dec(inp,key)
+
+def keymanage(key):
+	
+	cmd=""
+	while cmd!="back":
+		clear()
+		print('''===========================
+		KEY MANAGER
+===========================
+Generate -------------(new)
+Select ------------(select)
+===========================
+<--[back]
+===========================''')
+		cmd=lst_about(input(">> "),['new',"select","back"])
+		if cmd==('new'):
+			clear()
+			key1=gen_key()
+			filekey=open("newfilekey.nKey",'wb')
+			filekey.write(key1)
+			filekey.close()
+			print("The key was created. Do you want to set it as default?")
+			print("WARNING: This action will overwrite the default key.")
+			a=input("Y/N")
+			if a=='Y':
+				filekey=open("default.nKey",'wb')
+				filekey.write(key1)
+				filekey.close()
+				print("The key was set as default.")
+			else: 
+				print("The key was saved but will only be set as default for this session.")
+				time.sleep(0.5)
+			key=key1
+		elif cmd=="select":
+			file=explore()
+			filekey=open(file,'rb')
+			key1=filekey.read()
+			print("The key was set for this session. Do you want to set it as default?")
+			print("WARNING: This action will overwrite the default key.")
+			a=input("Y/N ")
+			time.sleep(0.5)
+			if a=='Y':
+				filekey=open("default.nKey",'wb')
+				filekey.write(key1)
+				filekey.close()
+
+				print("The key was set as default.")
+				time.sleep(0.5)
+			else: 
+				print("The key will only be set as default for this session.")
+				time.sleep(0.5)
+			key=key1
+		elif cmd=="back":
+			pass
+		else:
+			print("Invalid command")
+			time.sleep(0.5)
+	return key
+
+
+
+
+
+
---
+++ linux/file_explore.py
@@ -0,0 +1,144 @@
+
+import os, time
+
+def check(inp,exp):
+    ind=0
+    sim=0
+    while len(exp)<len(inp):
+        exp+="/"
+    while len(exp)>len(inp):
+        inp+="/"
+    exp+="/"
+    for i in inp:
+        if i== exp[ind]:
+            sim+=1
+        ind+=1
+
+    return sim
+def about(inp,expec,sim=50,rt="exp"):
+    exp=expec
+    inp.replace(" ","")
+
+    inp=inp.lower()
+    exp=exp.lower()
+    if len(exp)<len(inp):
+        inte=exp
+        exp=inp
+        inp=inte
+    sc=0
+    lsc=0
+    abs=len(exp)
+    for i in exp:
+        sc=check(inp,exp)
+        inp="/"+inp
+        if sc>lsc:
+            lsc=sc
+        
+    sc=lsc/abs*100
+    if rt=="exp":
+        if sc>= sim:
+            ret=expec
+        else:
+            ret=False
+    elif rt=="prc":
+        ret=sc
+    return ret
+def lst_about(inp,exp,sim=50):
+    lval=0
+    ret=False
+    if type(inp)==list:
+        for i in inp:
+            val=about(i,exp,sim)
+            if val!=False:
+                ret=val
+    elif type(exp)==list:
+        for i in exp:
+            val=about(inp,i,sim,"prc")
+            if val>lval:
+                lval=val
+                ret=i        
+    else:
+        ret=about(inp,exp,sim)
+
+    return ret        
+     
+def print_screen(path,find):
+    os.system('clear')
+    print("---------------------------------------------------------------")
+    print(path)
+    print("---------------------------------------------------------------")
+    ind=-1
+    for i in find:
+        ind+=1
+        print(ind," -- ",i)
+    print("---------------------------------------------------------------")
+    print("commands: |#back| |#done| |#reset| |#choose| |#i[index]| |item|")
+    print("---------------------------------------------------------------")
+    cmd=input(">> ")
+    if cmd[0]=="#" and cmd[1]=="i" :
+        cmd=cmd.replace(cmd[0],"")
+        cmd=cmd.replace(cmd[0],"")
+        cmd=int(cmd)
+        cmd=find[cmd]
+    elif cmd[0]=="#":
+        cmd= lst_about(cmd,['#back','#done','#reset',"#choose"])
+    else:
+        cmd=lst_about(cmd,find)
+        
+    return cmd
+def explore(base=""):
+    
+    cmd=""
+    if base!="":
+        pathcomp.append(base)
+    else :
+        pathcomp=[]
+    path=""
+    while cmd!="#done":
+        path=""
+        try:
+            if pathcomp==[]:
+                shpath="Base dir/"
+                find=os.listdir()
+            else:
+                path=""
+                for i in pathcomp:
+                    if path=="":
+                        path=i
+                    else:
+                        path=path+"/"+i
+                    shpath="Base dir/"+path
+                find=os.listdir(path)
+            cmd=print_screen(shpath,find)
+            if cmd[0]!="#":
+                try:
+                    if path == "":
+                        a= cmd
+                    else:
+                        a=path+"/"+cmd
+                    os.listdir(a)
+                    pathcomp.append(cmd)       
+                except:
+                    path=path+"/"+cmd
+                    cmd="#done"
+            else:
+                if cmd=="#back":
+                    pathcomp.remove(pathcomp[-1])
+                elif cmd=="#reset":
+                    cmd=""
+                    pathcomp=[]
+                    path=""
+                    a=int("w")
+                elif cmd=="#choose":
+                    base=input("Enter the path for the base directory: ")
+                    pathcomp=[base]
+        except:
+            print ('error') 
+            cmd=""
+            pathcomp=[]
+            path=""
+            time.sleep(0.5)  
+    if path[0]=="/" and base!= "":
+        path=path.replace(path[0],"",1)      
+    return path
+
